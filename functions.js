3a6e675a01719fcadef21d50eedf9722011aee84
seun jay <seunjay92@gmail.com>
2019-04-09 19:39:47 +0100

    Refactor add function to deal with cases of two 'stringed' numbers passed in

--- functions.js
+++ functions.js
@@ -1,7 +1,10 @@
 const functions = {
   add: function(a, b){
+    if(typeof a !== 'number' || typeof b !== 'number') {
+      return "Please enter two valid numbers";
+    }
     return a + b
   }
 }
 
-module.exports = functions
+module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -5,5 +5,5 @@ test('Adds 2 + 2 to equal 4', () => {
 });
 
 test('Adds "2" + "3" to return "Please enter two valid numbers"', () => {
-  expect(functions.add(2, 2)).toBe("Please enter two valid numbers")
+  expect(functions.add("2", "3")).toBe("Please enter two valid numbers")
 })
