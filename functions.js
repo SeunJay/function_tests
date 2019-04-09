50611d865969512b8b83d5ef756ab72384ae8f3b
seun jay <seunjay92@gmail.com>
2019-04-09 19:53:49 +0100

    Refactor add function to deal with cases of more than two valid numbers passed in

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
 const functions = {
   add: function(a, b){
+    if(arguments.length === 0) {
+      return "Arguments required";
+    } else if(arguments.length > 2){
+      return "Please entered just two valid numbers";
+    }
+
     if(typeof a !== 'number' || typeof b !== 'number') {
       return "Please enter two valid numbers";
     }
