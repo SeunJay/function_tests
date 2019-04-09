415798498bbb461bba179cc3b3c8620a27a2d816
seun jay <seunjay92@gmail.com>
2019-04-09 19:29:37 +0100

    Add case of two 'stringed' numbers passed in the function

--- functions.test.js
+++ functions.test.js
@@ -2,4 +2,8 @@ const functions = require('./functions')
 
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4 )
+});
+
+test('Adds "2" + "3" to return "Please enter two valid numbers"', () => {
+  expect(functions.add(2, 2)).toBe("Please enter two valid numbers")
 })
